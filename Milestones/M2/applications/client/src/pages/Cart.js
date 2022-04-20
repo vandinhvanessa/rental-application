f3512fe9bf7cae029c791a5484a15b1114611e04
Vanessa Van Dinh <vandinhvanessa@gmail.com>
2022-04-20 00:24:42 -0700

    Formated layout for cart page

--- Milestones/M2/applications/client/src/pages/Cart.js
+++ Milestones/M2/applications/client/src/pages/Cart.js
@@ -21,7 +21,7 @@ function Cart() {
         <div>
             {cart.map((value, key) => {
                 return (
-                    <div className="post" >
+                    /*<div className="post" >
                         <div className="title" onClick={() => {
                             navigate(`/post/${value.id}`, { replace: true })
                         }}> {value.title} </div>
@@ -41,9 +41,29 @@ function Cart() {
                             <div className="depositFee">Deposit Fee: {value.depositFee}</div>
                             <div className="shippingFee">Shipping Fee: {value.shippingFee}</div>
                             <div className="pricePerDay">$/Day: {value.pricePerDay}</div>
-
+                            <button className='buyButton' onClick={() => removeFromCart(value)} >Remove from Cart</button>
                         </div>
 
+                    </div>*/
+                    <div className="listing">
+                        <div className="listingLeft">
+                            <Image
+                                className="postImage"
+                                style={{ width: 450 }}
+                                cloudName="ditub0apw"
+                                publicId={value.image}
+                                onClick={() => {
+                                    navigate(`/post/${value.id}`, { replace: true })
+                                }}
+                            />
+                        </div>
+                        <div className="listingRight">
+                            <Link to={`/profile/${value.UserId}`}>{value.username}</Link>
+                            <div className="depositFee">Deposit Fee: {value.depositFee}</div>
+                            <div className="shippingFee">Shipping Fee: {value.shippingFee}</div>
+                            <div className="pricePerDay">$/Day: {value.pricePerDay}</div>
+                            <button className='buyButton' onClick={() => removeFromCart(value)} >Remove from Cart</button>
+                        </div>
                     </div>
 
                 );
